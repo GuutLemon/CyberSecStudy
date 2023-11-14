4edd7be3893d5bb8c0cee47308123d3bdeb9ff5e
Bob <supercowlevel@protonmail.com>
2023-11-14 03:28:51 -0800

    Weekly update

--- Python/Advent_of_codes/test.py
+++ Python/Advent_of_codes/test.py
@@ -1,12 +1,6 @@
-def points_with_equal_distance(center, distance):
-    result = []
-    for x in range(center[0] - distance, center[0] + distance + 1):
-        y_range = distance - abs(x - center[0])
-        # for y in range(center[1] - y_range, center[1] + y_range + 1):
-        result.append((x, y))
-    return result
+def test():
+    print('dog')
 
-center = (10, 10)
-distance = 2
-equal_distance_points = points_with_equal_distance(center, distance)
-print(equal_distance_points)
+a = input()
+print(a == 36)
+print(a)
